c712ee696983ab9cb5c3dc336d70b9257dd18fae
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-11 10:14:51 -0400

    Add genesis block to the blockchain

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,13 +1,27 @@
 import datetime
+from venv import create
 
 class BlockChain:
 
     def __init__(self) -> None:
         self.blockchain = []
-        pass
+        genesis_block = self.create_block(
+            index=1,
+            data="Genesis block",
+            proof=1,
+            prev_hash="0"
+        )
+        self.blockchain.append(genesis_block)
         
     def create_block(self, index:int, data:str, proof:int, prev_hash:str) -> dict:
-        pass
-
+        block = {
+            "index":index,
+            "timestamp":str(datetime.datetime.now()),
+            "proof":proof,
+            "data":data,
+            "prev_hash":prev_hash
+        }
+        return block
+        
     def mine_block(self, data:str) -> dict:
         pass
