be304a6dafd8fe3c6c2c2f504515925bac1b9c59
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-22 01:33:05 +0530

    Add endpoint to view blockchain

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
+from blockchain import BlockChain
 
 app = Flask(__name__)
+
+bc = BlockChain()
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/view_blockchain', methods=["GET"])
+def view_blockchain():
+    return render_template("view_blockchain.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
