095e68fcb7856806ab447483e68e65e0405d02bf
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-22 01:19:41 +0530

    Add endpoint to access homepage

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello world!"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
