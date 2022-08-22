4bace502043736fc0be968c5744e1efe7ef4152b
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-22 15:52:18 +0530

    Add endpoint to render homepage

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return "Hello world!"
+    if(request.method == "POST"):
+        pass
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
