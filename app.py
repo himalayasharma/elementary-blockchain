9733650378628cf122739323b319dd148b42da1b
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-22 15:59:43 +0530

    Add code to mine block in endpoint

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
+from blockchain import BlockChain
 
 app = Flask(__name__)
-
+bc = BlockChain()
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     if(request.method == "POST"):
-        pass
+        data = request.form['data_input']
+        bc.mine_block(data=data)
     return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
