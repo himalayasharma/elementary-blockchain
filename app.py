dd6a9dc13576208d5f4dc1a2c484964126620950
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-22 18:02:04 +0530

    Add code to send chain validity to homepage

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ def home():
     if(request.method == "POST"):
         data = request.form['data_input']
         bc.mine_block(data=data)
-    return render_template('home.html', blockchain=bc.blockchain)
+    is_chain_valid = bc.validate_chain()
+    if is_chain_valid:
+        validity_status = 'The chain is VALID'
+    else:
+        validity_status = 'The chain is INVALID'
+    return render_template('home.html', blockchain=bc.blockchain, validity_status=validity_status)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
