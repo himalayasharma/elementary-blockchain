efacafb99a634a82974ac5c88ad9401ea2ec4a08
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-08-25 17:07:49 +0530

    Enhance messages shown on modifying block

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import numpy as np
 from flask import Flask, request, render_template, flash, redirect
 from blockchain import BlockChain
 
+# Create app
 app = Flask(__name__)
+# Specify secret key (optional)
 app.secret_key = "password"
+# Instantiate blockchain
 bc = BlockChain()
 
 def get_validity_status(blockchain):
@@ -13,28 +17,41 @@ def get_validity_status(blockchain):
         validity_status = f'The chain is INVALID. {error["reason"]}'
     return validity_status
 
+# Base url for app is '/'
 @app.route('/', methods=["GET", "POST"])
 def home():
+    # `request_type` tells about the request made by MODIFY BLOCK
+    # By default it is set ""
     request_type = ""
     if(request.method == "POST"):
+        # CASE 1: MINE BLOCK
         if(request.form["action"] == "Create block"):
-            # Mine block
             data = request.form['data_input']
             bc.mine_block(data=data)
             return redirect("/")
+        # CASE 2: MODIFY BLOCK
         elif(request.form["action"] == "Update block"):
-            # Modify block
-            index = int(request.form['index_input'])
-            if(index>1 and index<=len(bc.blockchain)):
-                modified_data = request.form['modified_data_input']
-                bc.blockchain[index-1]["data"] = modified_data
-                request_type = "Request is okay"
-            elif(index == 1):
-                request_type = "Invalid request! Genesis block cannot be altered. Try again with some other index!"
+            index = request.form['index_input']
+            # INVALID REQUEST - if index is not a digit
+            if index.isnumeric() == False:
+                request_type = f"Invalid request! Please enter a number and try again. Valid indices are: {np.arange(2, len(bc.blockchain)+1)}"
+            # if index is a digit
             else:
-                request_type = f"Invalid request. Enter index only between 2 and current length of chain i.e. {len(bc.blockchain)}. Try again!"        
+                # VALID REQUEST - if index > 1 and < length of blockchain
+                # Cast index str into int
+                index = int(request.form['index_input'])
+                # Modify data in block with required index
+                if(index>1 and index<=len(bc.blockchain)):
+                    modified_data = request.form['modified_data_input']
+                    bc.blockchain[index-1]["data"] = modified_data
+                # INVALID REQUEST - genesis block i.e. the first block created in the blockchain by default is immutable
+                elif(index == 1):
+                    request_type = f"Invalid request! Genesis block cannot be altered. Try again with some other index. Valid indices are: {np.arange(2, len(bc.blockchain)+1)}"
+                # INVALID REQUEST - index specified is more than length of the chain
+                else:
+                    request_type = f"Invalid request! Valid indices are: {np.arange(2, len(bc.blockchain)+1)}"        
+        # CASE 3: RESET BLOCKCHAIN
         else:
-            # Reset blockchain
             bc.blockchain = BlockChain().blockchain
 
     return render_template('home.html', blockchain=bc.blockchain, validity_status=get_validity_status(bc), request_type=request_type)
